52b4763be4949ee17db8fb299bcb79efdef3472b
Claudiu <haidu_claudiu@yahoo.com>
2017-12-06 23:07:10 +0200

    added after method

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -30,6 +30,11 @@ public class BaseTest {
         driver.get("http://localhost:3000/");
     }
 
+      /*@AfterMethod(alwaysRun = true)
+    public void closeBrowser() {
+        driver.quit();
+    }*/
+
     public static void clickElement(WebElement webElement) {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("var evt ="+
